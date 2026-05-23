e143281b9f5b8f851831881ca21f6ba19aa5cf2d
Rui Martins <102309086+RuiAlvesMartins@users.noreply.github.com>
2026-05-23 11:50:23 +0100

    IMPORTANT: implementation of .gitignore

---
+++ src/gitIgnoreTest/Main.java
@@ -0,0 +1,14 @@
+package gitIgnoreTest;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is a test to see if .gitignore is working properly!");
+
+
+        System.out.println("Test 2");
+        System.out.println("Test 3");
+    }
+
+}
